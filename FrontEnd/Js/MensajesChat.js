70003f55145b63a5331e7ec50eb09d9a3de11b70
JuanJ189 <165536496+JuanJ189@users.noreply.github.com>
2025-10-07 22:16:54 -0400

    Subida
    Subida

--- FrontEnd/Js/MensajesChat.js
+++ FrontEnd/Js/MensajesChat.js
@@ -21,3 +21,72 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById("Resumen").innerText = "No se pudo cargar el resumen";
     }
 });
+
+//Seccion de chat
+const Mensajes = document.getElementById('messages');
+const inputEl = document.getElementById('inputMsg');
+const btnenviar = document.getElementById('sendBtn');
+const estado = document.getElementById('estado');
+
+function AgregarMensaje(text, who){
+    const wrap = document.createElement('div');
+
+    wrap.className = 'msg' + who;
+    wrap.innerHTML = `<div class=text> ${text}</div>`;
+    Mensajes.appendChild(wrap);
+    Mensajes.scrollTo = Mensajes.scrollHeight;
+}
+
+function escapeHtml(texto){
+    return texto.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/\"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+async function EnviarMensaje(){
+    const texto = inputEl.value.trim();
+
+    if(!texto) return;
+    AgregarMensaje(texto, 'user');
+    inputEl.value = '';
+    inputEl.focus();
+    estado.textContent = 'Escribiendo ...';
+
+    try{
+        
+        const response = await fetch(`http://127.0.0.1:8000/Chat/?mensaje=${texto}`);
+        if (!response.ok) {
+            throw new Error("Error en la llamada: " + response.status);
+        }
+
+        const data = await response.json();
+
+        console.log("mensaje recibido: " + data.mensaje)
+
+        const respuestaHTML = marked.parse(data.mensaje);
+
+        AgregarMensaje(respuestaHTML, 'bot');
+
+
+
+    }
+    catch(Error){
+       console.log("Ocurrio un error en el mensaje". Error)
+
+    }
+    finally{
+        estado.textContent = 'Listo';
+    }
+}
+
+btnenviar.addEventListener('click', EnviarMensaje);
+
+inputEl.addEventListener('keydown', (e)=>{
+    if (e.key === 'Enter'){
+        e.preventDefault();
+        EnviarMensaje();
+    }
+});
+
